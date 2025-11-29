65b8d2e428dcf2a177511876bb69c5712406febc
Mon732 <mon_732@yahoo.co.uk>
2025-11-29 18:48:32 +0000

    Fixated, lost track of the changes I made

--- visors.gd
+++ visors.gd
@@ -1,14 +1,26 @@
 extends CanvasLayer
 
 @export var camera : Camera3D
+
 @onready var thermal : Control = $"Thermal"
+@onready var echo : Control = $"Echo"
 
 const BITMASK_THERMAL = 2
+const BITMASK_ECHO = 4
+const BITMASK_NORMAL = ~(BITMASK_THERMAL | BITMASK_ECHO)
 
 func _input(_event):
 	if (Input.is_action_just_pressed("visor_1")):
 		thermal.visible = false
-		camera.cull_mask &= ~BITMASK_THERMAL
+		echo.visible = false
+		camera.cull_mask = BITMASK_NORMAL
 	elif (Input.is_action_just_pressed("visor_2")):
 		thermal.visible = true
+		echo.visible = false
+		camera.cull_mask = BITMASK_NORMAL
 		camera.cull_mask |= BITMASK_THERMAL
+	elif (Input.is_action_just_pressed("visor_3")):
+		thermal.visible = false
+		echo.visible = true
+		camera.cull_mask = BITMASK_NORMAL
+		camera.cull_mask |= BITMASK_ECHO
