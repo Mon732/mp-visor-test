238088db4ed52490ba42338b873a7d3e92dbfc4e
Mon732 <mon_732@yahoo.co.uk>
2025-12-03 15:09:19 +0000

    Directory restructure Files are grouped into loosely coupled modules

--- main.gd
+++ main.gd
@@ -6,8 +6,8 @@ extends Node3D
 @onready var player : Player = $"Player"
 
 func _ready():
-    player.visor_changed.connect(on_visor_changed)
+	player.visor_changed.connect(on_visor_changed)
 
 func on_visor_changed(visor:int):
-    world.on_visor_changed(visor)
-    visor_layer.on_visor_changed(visor)
+	world.on_visor_changed(visor)
+	visor_layer.on_visor_changed(visor)
