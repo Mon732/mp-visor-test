ca9dd6609a248955134b9d88411eae6f56c6efaa
Mon732 <mon_732@yahoo.co.uk>
2025-12-03 18:27:58 +0000

    Moved fps cap to Game autoload

--- game.gd
+++ game.gd
@@ -1,3 +1,6 @@
 extends Node
 
 enum Visors {VISOR_NONE, VISOR_THERMAL, VISOR_ECHO}
+
+func _ready():
+    Engine.max_fps = 300
--- ui/ui.gd
+++ ui/ui.gd
@@ -2,10 +2,5 @@ extends CanvasLayer
 
 @onready var fps_label : Label = $"FPS"
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	Engine.max_fps = 300
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	fps_label.text = "FPS %d" % Engine.get_frames_per_second()
